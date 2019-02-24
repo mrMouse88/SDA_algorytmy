370ee90452d27a4dadb30ab3f9badada9357e26e
mrMouse <ia@asygnata.com>
2019-02-24 14:00:27 +0100

    Exercise2

--- src/main/java/org/sda/algorithms/exercises/Exercise2.java
+++ src/main/java/org/sda/algorithms/exercises/Exercise2.java
@@ -3,9 +3,11 @@ package org.sda.algorithms.exercises;
 
 import org.sda.algorithms.util.CalcTime;
 
+import java.util.Arrays;
+
 /**
  * mark
- *
+ * <p>
  * Stwórz tablicę posiadającą 10 elementów, które są liczbami typu int.
  * Wyświetl w pętli zawartość listy.
  * Następnie odwróć kolejność zapisanych elementów w tablicy.
@@ -18,8 +20,16 @@ public class Exercise2 {
 
         CALC_TIME.start();
 
-        // TODO
-        // napisz tutaj logike
+        int[] numb = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            numb[i] = i + 1;
+        }
+
+        printArray(numb);
+//        numb = reverseArray(numb);
+        numb = betterReverseArray(numb);
+        printArray(numb);
 
         CALC_TIME.stop();
         CALC_TIME.display();
@@ -31,6 +41,26 @@ public class Exercise2 {
      * napisz implementacje ktora wydrukuje liste w konsoli
      */
     private static void printArray(int[] numbers) {
+        for (int x : numbers) {
+            System.out.println(x);
+        }
+    }
 
+    private static int[] reverseArray(int[] numbers) {
+        int[] reverse = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            reverse[i] = numbers[numbers.length - 1 - i];
+        }
+        return reverse;
+    }
+
+    private static int[] betterReverseArray(int[] numbers) {
+        for(int i = 0; i < numbers.length / 2; i++)
+        {
+            int temp = numbers[i];
+            numbers[i] = numbers[numbers.length - i - 1];
+            numbers[numbers.length - i - 1] = temp;
+        }
+        return numbers;
     }
 }
